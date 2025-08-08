9919f3b719d7f4fe53c1fdad40255e878bd83248
leandroti7 <leandrosouza.dasilva14@gmail.com>
2025-08-08 01:18:03 -0300

    crate moment at of building the block - create count payment to link - crate function to only admin make updates on comission - create function to only admin delete owner link

--- contracts/LinkShield.sol
+++ contracts/LinkShield.sol
@@ -9,11 +9,40 @@ contract LinkShield {
         string url;
         address owner;
         uint256 fee;
+        uint256 sales;
+        uint256 atCreate;
     }
 
     uint256 public commission = 1;
     mapping (string => Link) private links; // Mapping para mapear cada objeto link
     mapping (string => mapping ( address => bool)) public hasAccess; // mapping para verificar se o usuario tem acesso
+    uint256 public creationTimestamp;
+    address admin = address(0);
+
+    constructor() {
+        creationTimestamp = block.timestamp;
+        admin = msg.sender;
+    }
+
+    function deleteLlink(string calldata linkId) public  {
+        require(msg.sender == admin, "Unauthorized");
+        delete links[linkId];
+    }
+
+    function setComi(uint256 newComission) public {
+        require(msg.sender == admin, "Unauthorized");
+        commission = newComission;
+    }
+
+    function getHoursMinSec(string calldata linkId) public view returns  (Link memory) {
+        Link memory link = links[linkId];
+        uint totalSeconds = creationTimestamp % 86400;
+        uint8 hour = uint8(totalSeconds / 3600);
+        uint8 minute = uint8(totalSeconds %  3600 / 60);
+        
+        link.atCreate = hour;
+        return link;
+    }
 
     function addLink(string calldata url, string calldata linkId, uint256 fee) public {
         Link memory link = links[linkId];
@@ -33,6 +62,9 @@ contract LinkShield {
         require(link.owner != address(0), "This linkId not found"); // Verifica se o link existe
         require(hasAccess[linkId][msg.sender] == false, "You alread has access"); // Verifica se o usuario ja tem acesso
         require(msg.value >= link.fee, "Insufficient payment"); // Verifica se ja foi pago
+
+        links[linkId].sales += msg.value - commission; // Atualiza o valor das vendas do link
+        links[linkId].sales++;
 
         hasAccess[linkId][msg.sender] = true;
         payable(link.owner).transfer(msg.value - commission); // Transfere para o dono do link) 
